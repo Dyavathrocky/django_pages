725ebebb151b37a302d1ebc26ec10d36f4c778bb
Dyavathrocky <davathrak@gmail.com>
2020-05-26 07:20:11 +0530

    post app

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig', #adding pages app
+    'posts.apps.PostsConfig', #adding posts app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'hello_world.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR , 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ posts/urls.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.urls import path , include
+from .views import HomePageView
+
+urlpatterns =[
+    path('',HomePageView.as_view() , name='home')
+]
